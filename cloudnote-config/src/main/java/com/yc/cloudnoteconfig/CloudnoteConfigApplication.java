218370002dd0a30f44465de4855e630cd0ff936c
yangliu-zhao <1104437196@qq.com>
2021-06-03 16:19:03 +0800

    服务注册+配置中心

--- cloudnote-config/src/main/java/com/yc/cloudnoteconfig/CloudnoteConfigApplication.java
+++ cloudnote-config/src/main/java/com/yc/cloudnoteconfig/CloudnoteConfigApplication.java
@@ -2,8 +2,10 @@ package com.yc.cloudnoteconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudnoteConfigApplication {
 
 	public static void main(String[] args) {
