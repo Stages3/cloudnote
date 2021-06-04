ccb593c79994e4bc2b5b741f69173da59062449c
yangliu-zhao <1104437196@qq.com>
2021-06-04 18:31:20 +0800

    master

--- cloudnote-zuul/src/main/java/com/yc/cloudnotezuul/CloudnoteZuulApplication.java
+++ cloudnote-zuul/src/main/java/com/yc/cloudnotezuul/CloudnoteZuulApplication.java
@@ -2,8 +2,10 @@ package com.yc.cloudnotezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy    //开启路由
 public class CloudnoteZuulApplication {
 
 	public static void main(String[] args) {
